ac53494e7985ef94af015652928efb063564b752
Onur Akdogan <laoth@MacBookAir.hgw.local>
2025-06-10 15:45:57 +0300

    Update firebase config to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getFunctions } from "firebase/functions";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2Da-IjqzfNzA7KobhlR9OmCMBf-nwUi0",
-  authDomain: "simulate-wallet.firebaseapp.com",
-  projectId: "simulate-wallet",
-  storageBucket: "simulate-wallet.firebasestorage.app",
-  messagingSenderId: "435522380162",
-  appId: "1:435522380162:web:b5cef5655bb5eb5272af9b",
-  measurementId: "G-BWYZ1DK158"
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
